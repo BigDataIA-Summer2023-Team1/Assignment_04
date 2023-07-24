1a22f832da83e1153421e927c9687a1fb3c968b8
sanjanakarra9 <karra.san@northeastern.edu>
2023-07-24 18:02:59 -0400

    Adds images

--- frontend/main.py
+++ frontend/main.py
@@ -25,44 +25,44 @@ st.markdown("""
     """)
 
 st.title('Store Sales ER-Diagram')
-image_path = '../Images/Store_Sales.png'  
+image_path = './Images/Store_Sales.png'  
 st.image(image_path, caption='Store Sales ER Diagram', use_column_width=True)
 st.markdown("""Each row in this table represents a single lineitem for a sale made through the store channel and recorded in the
 store_sales fact table.""")
 
 st.title('Store Returns ER-Diagram')
-image_path = '../Images/Store_returns.png'  
+image_path = './Images/Store_returns.png'  
 st.image(image_path, caption='Store Returns ER Diagram', use_column_width=True)
 st.markdown("""Each row in this table represents a single lineitem for the return of an item sold through the store channel and
 recorded in the store_returns fact table.""")
 
 st.title('Catalog Sales ER-Diagram')
-image_path = '../Images/Catalog_Sales.png'  
+image_path = './Images/Catalog_Sales.png'  
 st.image(image_path, caption='Catalog Sales ER Diagram', use_column_width=True)
 st.markdown("""Each row in this table represents a single lineitem for a sale made through the catalog channel and recorded in
 the catalog_sales fact table.""")
 
 st.title('Catalog Returns ER-Diagram')
-image_path = '../Images/Catalog_Returns.png'  
+image_path = './Images/Catalog_Returns.png'  
 st.image(image_path, caption='Catalog Returns ER Diagram', use_column_width=True)
 st.markdown("""Each row in this table represents a single lineitem for the return of an item sold through the catalog channel and
 recorded in the catalog_returns table.""")
 
 st.title('Web Sales ER-Diagram')
-image_path = '../Images/Web_Sales.png'  
+image_path = './Images/Web_Sales.png'  
 st.image(image_path, caption='Web Sales ER Diagram', use_column_width=True)
 st.markdown("""Each row in this table represents a single lineitem for a sale made through the web channel and recorded in the
 web_sales fact table.""")
 
 st.title('Web Returns ER-Diagram')
-image_path = '../Images/Web_Returns.png'  
+image_path = './Images/Web_Returns.png'  
 st.image(image_path, caption='Web Returns ER Diagram', use_column_width=True)
 st.markdown("""Each row in this table represents a single lineitem for the return of an item sold through the web sales channel
 and recorded in the web_returns table.
 """)
 
 st.title('Inventory ER-Diagram')
-image_path = '../Images/Inventory.png'  
+image_path = './Images/Inventory.png'  
 st.image(image_path, caption='Inventory', use_column_width=True)
 st.markdown("""Each row in this table represents the quantity of a particular item on-hand at a given warehouse during a
 specific week.""")
